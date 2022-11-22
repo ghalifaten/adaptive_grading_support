54311f310db5a2cb7537f4db0d1a846732431126
ghalifaten <faten.ghali@epfl.ch>
2022-11-22 22:24:58 +0100

    implement spelling check feature.

--- graspee-interface/front-end/src/main.py
+++ graspee-interface/front-end/src/main.py
@@ -7,6 +7,13 @@ import neuspell
 from neuspell import BertChecker
 ##################
 
+########## Language Models #########
+# Initialize spelling check model
+checker = BertChecker()
+checker.from_pretrained()
+
+#####################################
+
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET','POST'])
@@ -29,10 +36,22 @@ def grading():
         sub_grade_4 = request.form['sub-grade-4']
         return redirect(url_for("summary", feedback=feedback, sub_grade_1=sub_grade_1, sub_grade_2=sub_grade_2, sub_grade_3=sub_grade_3, sub_grade_4=sub_grade_4))
 
-    text = request.args.get('text', None)
+    original_text = request.args.get('text', None)
     assignment_nbr = request.args.get('assignment_nbr', None)
     student_name = request.args.get('student_name', None)
-    return render_template('grading.html', text=text, assignment_nbr=assignment_nbr, student_name=student_name)
+
+    #Spell check
+    correct_text = checker.correct(original_text)
+
+    data = {
+        'student_name':student_name,
+        'assignment_nbr':assignment_nbr,
+        'original_text':original_text,
+        'correct_text':correct_text
+    }
+    return render_template('grading.html', data=data)
+
+
 
 @app.route('/summary')
 def summary():
@@ -50,6 +69,7 @@ def predict():
     '''
     if request.method == 'GET':
         return render_template('predict.html')
+        
     inp_string = [x for x in request.form.values()]
     sent = inp_string[0]
     index = bert.bert_checker(sent)
@@ -57,25 +77,6 @@ def predict():
     return render_template('predict.html', sent=sent, index=index.item())
 
 
-# Initialize spelling check model
-checker = BertChecker()
-checker.from_pretrained()
-
-@app.route('/predict_neuspell', methods=['GET','POST'])
-def predict_neuspell():
-    '''
-    For rendering results on HTML GUI
-    '''
-    if request.method == 'GET':
-        return render_template('predict_neuspell.html', data={'original_sentence':'', 'corrected_sentence':''})
-    
-    inp_string = [x for x in request.form.values()]
-    sent = inp_string[0]
-    correct_sent = checker.correct(sent)
-    data = {'original_sentence':sent, 'corrected_sentence':correct_sent}
-    print(data['original_sentence'])
-    print(data['corrected_sentence'])
-    return render_template('predict_neuspell.html', data=data, correct_sent=correct_sent)
 
 if __name__ == "__main__":
     app.run(debug=True)
