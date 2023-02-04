6ede4b8b6df5ed5b7979b4ecb92b4b0a2ace240b
ghalifaten <faten.ghali@epfl.ch>
2023-02-04 21:12:07 +0100

    interface code cleanup

--- graspe-interface/front-end/src/main.py
+++ graspe-interface/front-end/src/main.py
@@ -3,7 +3,7 @@ import os, sys
 import time
 import math
 import re
-##################
+
 import neuspell
 from neuspell import BertChecker
 from transformers import BertForSequenceClassification, BertTokenizer
@@ -30,90 +30,16 @@ text_classifier = pipeline("sentiment-analysis", model="fatenghali/text_classifi
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/index', methods=['GET','POST'])
-def index():
-    if  request.method == 'POST':
-        assignment_nbr = request.form['assignment-nbr']
-        student_name = request.form['student-name']
-        text = request.form['text-body']
-        return redirect(url_for("grading", text=text, assignment_nbr=assignment_nbr, student_name=student_name))
-    
-    return render_template("index.html")
-
-@app.route('/grading', methods=['GET', 'POST'])
-def grading():
-    """ 
-    if  request.method == 'POST':
-        feedback = request.form['feedback-text']
-        sub_grade_1 = request.form['sub-grade-1']
-        sub_grade_2 = request.form['sub-grade-2']
-        sub_grade_3 = request.form['sub-grade-3']
-        sub_grade_4 = request.form['sub-grade-4']
-        return redirect(url_for("summary", feedback=feedback, sub_grade_1=sub_grade_1, sub_grade_2=sub_grade_2, sub_grade_3=sub_grade_3, sub_grade_4=sub_grade_4))
-
-    original_text = request.args.get('text', None)
-    assignment_nbr = request.args.get('assignment_nbr', None)
-    student_name = request.args.get('student_name', None)
-
-    #Spell check
-    spell_correct_text = checker.correct(original_text)
-
-    #grammar check
-    list_sentences = original_text.split('.')
-    list_sentences = list(filter(None, list_sentences))
-    indices = list(map(lambda sent: bert.bert_checker(model_loaded, tokenizer, sent), list_sentences))
-    indices = list(map(lambda index: index.item(), indices))
-    grammar_check_results = dict(zip(list_sentences, indices))
-    print(grammar_check_results)
-
-    #-------
-    data = {
-        'student_name':student_name,
-        'assignment_nbr':assignment_nbr,
-        'original_text':original_text,
-        'spell_correct_text':spell_correct_text,
-        'grammar_check_results':grammar_check_results
-    }
-    """
-    return render_template('grading.html', data=data)
-
-@app.route('/comparison')
-def comparison():
-    return render_template('comparison.html')
-
-@app.route('/summary')
-def summary():
-    feedback = request.args.get('feedback', None)
-    sub_grade_1 = request.args.get('sub_grade_1', None)
-    sub_grade_2 = request.args.get('sub_grade_2', None)
-    sub_grade_3 = request.args.get('sub_grade_3', None)
-    sub_grade_4 = request.args.get('sub_grade_4', None)
-    return render_template('summary.html', feedback=feedback, sub_grade_1=sub_grade_1, sub_grade_2=sub_grade_2, sub_grade_3=sub_grade_3, sub_grade_4=sub_grade_4)
+## STUDENT INTERFACE ##
+@app.route('/submit', methods=['GET','POST'])
+def submit():    
+    return render_template("submit.html")
 
 
-
-
-@app.route('/predict', methods=['GET','POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    if request.method == 'GET':
-        return render_template('predict.html')
-    
-    start = time.time()
-    inp_string = [x for x in request.form.values()]
-    sent = inp_string[0]
-    list_sentences = sent.split('.')
-    list_sentences = list(filter(None, list_sentences))
-
-    indices = list(map(lambda sent: bert.bert_checker(model_loaded, tokenizer, sent), list_sentences))
-    indices = list(map(lambda index: index.item(), indices))
-    res_prediction = dict(zip(list_sentences, indices))
-    return render_template('predict.html', sent=sent, res_prediction=res_prediction)
-
-###############
 def uncommonWords(text, correct):
+    """
+    find the list of uncommon words between two texts.
+    """
     count = {}
     # insert words of string A to hash
     for word in text.split():
@@ -126,6 +52,7 @@ def uncommonWords(text, correct):
     # return required list of words
     return [word for word in count if count[word] > 0 ]
 
+## TEACHER INTERFACE ##
 @app.route('/', methods=['GET', 'POST'])
 def grade_compare():
     if request.method == 'GET':
@@ -207,7 +134,6 @@ def grade_compare():
             'predictions': predictions,
         }
 
-        print("\n DATA IS SENT FROM FLASK \n")
         return jsonify(data)
 
 
