6f5cf64957bc7827cc778013f411272efc61ad55
ghalifaten <faten.ghali@epfl.ch>
2023-02-09 13:29:21 +0100

    minor changes

--- graspe-interface/front-end/src/main.py
+++ graspe-interface/front-end/src/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 import os, sys
-import time
 import math
 import re
 
@@ -60,20 +59,13 @@ def grade_compare():
         return render_template("grading_comparison.html")
 
     if request.method == 'POST':
-        s1 = time.time()
         original_text = request.get_json()['original_text']
-        print("\n text received by Flask : ", time.time() - s1, "\n")
         """
         Spelling check
         """
-        s2 = time.time()
         #passing batches of sentences instead of large text avoids text truncation when text is too long for the model.
         batches = original_text.split('. ')
-        print("split: ", time.time() - s2)
-        ss2 = time.time()
         corrected_batches = checker.correct_strings(batches)
-        print("checker: ", time.time() - ss2)
-        sss2 = time.time()
         #Neuspell separates words from punctuation and inserts space before each in the returned result. So we fix that here to make the corrected text prettier and easier to compare to the original.
         for i in range(len(corrected_batches)):
             sentence = corrected_batches[i]
@@ -89,24 +81,20 @@ def grade_compare():
                                 .replace(' ) ', ') ')\
                                 .replace(' .', '.')
             corrected_batches[i] = sentence
-        print("for loop: ", time.time() - sss2)
         #Compare between each couple of sentences (original and corrected) to find uncommon words, aka spelling mistakes
         mistakes_nbr = 0
         highlighted_text = ''
-        ssss2 = time.time()
+
         for (orig, corr) in zip(batches, corrected_batches):
             mistakes = uncommonWords(orig, corr)
             mistakes_nbr += len(mistakes)
             for m in mistakes:
                 orig = orig.replace(m, '<span style="color: red">'+m+'</span>')
             highlighted_text += orig + '. ' 
-        print("2nd for loop: ", time.time() - ssss2)
-        print("\n SPELLING CHECK IS DONE: ", time.time() - s2, "\n")
 
         """
         Grammar check
         """
-        s3 = time.time()
         sentences = original_text.split('. ')
         sentences = list(filter(None, sentences))
         indices = list(map(lambda sent: bert.bert_checker(model_loaded, tokenizer, sent), sentences))
@@ -114,19 +102,14 @@ def grade_compare():
 
         grammar_check_results = [(sentences[i], indices[i]) for i in range(0, len(sentences))] #merge sentences and corresponding indices in list of tuples to keep order of sentences when sent to JavaScript
 
-        print("\n GRAMMAR CHECK IS DONE: ", time.time() - s3, "\n")
-
         """
         Argumentation check
         """
-        s4 = time.time()
         sentences = original_text.split('. ')
         predictions = text_classifier(sentences)
         for pred, sent in zip(predictions, sentences): #append sentences to their labels
             pred['sentence'] = sent
         
-
-        print("\n ARGUMENTATION CHECK IS DONE: ", time.time() - s4, "\n")
 
         #-------
         data = {
